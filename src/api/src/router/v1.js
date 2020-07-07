b87044502fe3444f691654efbfcaae95bcbfec59
Linus Jansson <00jansson@gmail.com>
2020-07-07 16:20:48 +0200

    Fix bootsteap

--- src/api/src/router/v1.js
+++ src/api/src/router/v1.js
@@ -4,5 +4,10 @@ let router = require('express').Router()
 
 router.get('/', (req, res) => { res.send("eyyy") });
 
+router.get('/callback', (req, res) => {
+    console.log(req);
+    res.send('OK')
+})
+
 
 module.exports = router;
